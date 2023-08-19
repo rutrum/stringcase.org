22274b1984595e332e4843c96e15fad67664446c
David Purdum <purdum41@gmail.com>
2023-08-19 14:38:50 -0400

    Add a flake for dev environment

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "stringcase.org flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        just
+        zola
+      ];
+    };
+  };
+}
